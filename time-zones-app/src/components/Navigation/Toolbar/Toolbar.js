c106a7073bb4e025c40faca317a8c3a4c58c6ea5
andrii medytskyi <medandriy@gmail.com>
2019-09-29 01:22:27 +0300

    UI: Added basic react layout

---
+++ time-zones-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Toolbar.module.css';
+
+const Toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        Some Toolbar...
+    </header>
+);
+
+export default Toolbar;
