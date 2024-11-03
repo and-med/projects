bca1819ee73a020b398e9fd5398426b7cc6803e5
h0neyMan <medandriy@gmail.com>
2024-11-03 15:19:03 +0100

    removing invocation of firebase

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -1,11 +1,10 @@
 import { put } from 'redux-saga/effects';
 
-import axios from '../../axios-orders';
 import * as actions from '../actions/index';
 
 export function* initIngredientsSaga(action) {
     try {
-        const response = yield axios.get('/ingredients.json');
+        const response = {data: {salad: 2, bacon: 1, cheese: 1, meat: 1}};
         yield put(actions.setIngredients(response.data));
     } catch (error) {
         yield put(actions.fetchIngredientsFailed());
